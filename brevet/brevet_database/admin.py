c5cfe912f604f8920a26def273b3a82adbd32d68
Raccoonrider <avermakov@list.ru>
2024-05-20 13:52:33 +0600

    Fix search in ApplicationModelAdmin

--- brevet/brevet_database/admin.py
+++ brevet/brevet_database/admin.py
@@ -60,7 +60,7 @@ class ApplicationAdmin(admin.ModelAdmin):
     model = Application
     raw_id_fields = ['result']
     autocomplete_fields = ['user', 'event']
-    search_fields = ('user__last_name', 'event__route')
+    search_fields = ('user__last_name', 'event__route__name')
     list_display = ('user__name', 'event', 'active')
     list_filter = ('active', 'dnf', 'dns', 'dsq', 'otl', 'payment')
 
