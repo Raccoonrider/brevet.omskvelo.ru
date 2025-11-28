7d9363ee7157babf397d039b436192dc978b03d2
Raccoonrider <avermakov@list.ru>
2025-11-28 22:26:54 +0600

    Update InventoryAdmin

--- brevet/inventory/admin.py
+++ brevet/inventory/admin.py
@@ -1,4 +1,6 @@
+from django.utils import timezone
 from django.contrib import admin
+from django.contrib import messages
 
 from inventory.models import Medal, Price
 
@@ -26,4 +28,83 @@ class MedalAdmin(admin.ModelAdmin):
     def get_event_date(self, instance:Medal):
         return instance.event.date
     
+    def generic_action(self, request, queryset, action:callable, success_text, error_text):
+        fails = []
+        for item in queryset:
+            try:
+                action(item)
+            except:
+                fails.append(str(item))
+
+        if fails:
+            self.message_user(
+                request,
+                f"{error_text}: {', '.join(fails)}",
+                messages.ERROR,
+            )
+        else:
+            self.message_user(
+                request,
+                f"{success_text}: {len(queryset)}",
+                messages.SUCCESS,
+            )
+    
+
+    
+    @admin.action(description="Отметить заказ")
+    def action_order(self, request, queryset):
+        def action(item:Medal):
+            if item.ordered is None:
+                item.ordered = timezone.now()
+                item.save()
+
+        return self.generic_action(
+            request, queryset, action,
+            success_text="Успешно заказаны",
+            error_text="Ошибка"
+        )
+
+
+    @admin.action(description="Отметить приём")
+    def action_order(self, request, queryset):
+        def action(item:Medal):
+            if item.received is None:
+                item.received = timezone.now()
+                item.save()
+
+        return self.generic_action(
+            request, queryset, action,
+            success_text="Успешно приняты",
+            error_text="Ошибка"
+        )
+    
+
+    @admin.action(description="Отметить оплату")
+    def action_order(self, request, queryset):
+        def action(item:Medal):
+            if not item.payment:
+                item.payment = True
+                item.save()
+
+        return self.generic_action(
+            request, queryset, action,
+            success_text="Успешно заказаны",
+            error_text="Ошибка"
+        )
+    
+
+    @admin.action(description="Отметить вручение")
+    def action_order(self, request, queryset):
+        def action(item:Medal):
+            if item.removed is None:
+                item.removed = timezone.now()
+                item.save()
+
+        return self.generic_action(
+            request, queryset, action,
+            success_text="Успешно вручены",
+            error_text="Ошибка"
+        )
+    
+
 admin.site.register(Price)
